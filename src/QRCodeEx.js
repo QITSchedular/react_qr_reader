3b5c0781d2c88b76480721e5c2d9186fba8faf3b
QITSchedular <qitschedular2023@gmail.com>
2023-04-10 13:26:17 +0530

    first commit

---
+++ src/QRCodeEx.js
@@ -0,0 +1,68 @@
+// import React, { useState } from "react";
+// // working version npm i react-qr-reader@2.2.1 --force
+
+// const QRCodeEx = () => {
+//   const [text, useState] = useState();
+//   return (
+//     <div className="container mx-auto mt-2">
+//       <div className="row">
+//         <h2 className="col-sm-12 badges bg-success text-center text-white">
+//           Qr Generator
+//         </h2>
+//       </div>
+//       <div className="row">
+//         <h3 className="col-sm-12 text-center">Enter ul</h3>
+//       </div>
+//       <div className="row">
+//         <input
+//           type="text"
+//           className="col-sm-8"
+//           value={"text"}
+//           onChange={(e) => setText(e.target.value)}
+//         />
+//       </div>
+//       <button
+//         className="col-sm-2 btn btn-primary m-2"
+//         onClick={() => {}}
+//       ></button>
+
+//       <div className="row">
+//         <div className="card col-sm-4 m-2">
+//           <div className="card-header m-1 rounded">
+//             <h3 className="badges bg-seconday rounded text-center text-danger">
+//               QR IMG
+//             </h3>
+//           </div>
+//           <div className="card-body text-center">
+//           <img src="" alt="qrcode pic is here" />
+//         </div>
+//         </div>
+
+//         <div className="card col-sm-4 m-2">
+//           <div className="card-header m-1 rounded">
+//             <h3 className="badges bg-seconday rounded text-center text-danger">
+//               QR IMG
+//             </h3>
+//           </div>
+//           <div className="card-body text-center">
+//           <img src="" alt="qrcode pic is here" />
+//         </div>
+//         </div>
+
+//         <div className="card col-sm-4 m-2">
+//           <div className="card-header m-1 rounded">
+//             <h3 className="badges bg-seconday rounded text-center text-danger">
+//               QR IMG
+//             </h3>
+//           </div>
+//           <div className="card-body text-center">
+//           <img src="" alt="qrcode pic is here" />
+//         </div>
+//         </div>
+        
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default QRCodeEx;
